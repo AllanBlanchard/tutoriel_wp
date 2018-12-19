3da0946a53e1cd67f0c6bc319ae5f07a48974af8
Allan Blanchard <allan.blanchard@inria.fr>
2018-12-19 15:23:50 +0100

    Adds an exaplanations about abrupt termination of loops

---
+++ code/statements/loops/first_loop-non-term-2.c
@@ -0,0 +1,17 @@
+int main(){
+  int i = 0;
+  int h = 42;
+  
+  /*@
+    loop invariant 0 <= i <= 29;
+    loop assigns i;
+    loop variant 30 - i;
+  */
+  while(i < 30){
+    ++i;
+
+    if(i == 30) break ;
+  }
+  //@assert i == 30;
+  //@assert h == 42;
+}
---
+++ code/statements/loops/first_loop-non-term-3.c
@@ -0,0 +1,21 @@
+int main(){
+  int i = 0;
+  int h = 42;
+  
+  /*@
+    loop invariant 0 <= i <= 29;
+    loop assigns i;
+    loop variant 30 - i;
+  */
+  while(i < 30){
+    ++i;
+
+    if(i == 30){
+      i = 42 ;
+      h = 84 ;
+      break ;
+    }
+  }
+  //@assert i == 42;
+  //@assert h == 84;
+}
