cf131f66c5c8cbc9ef9c46f14aa2144ec08b456f
Allan Blanchard <allan.blanchard@inria.fr>
2019-05-17 10:15:33 +0200

    Adds a new exercise in 3.3

---
+++ code/function-contract/behaviors/ex-6-order-3-answer.c
@@ -0,0 +1,48 @@
+/*@
+  requires \valid(a) && \valid(b) && \valid(c) ;
+  requires \separated(a, b, c);
+
+  assigns *a, *b, *c ;
+
+  ensures *a <= *b <= *c ;
+  ensures { *a, *b, *c } == \old({ *a, *b, *c }) ;
+
+  behavior all_same:
+    assumes *a == *b == *c ;
+    ensures *a == *b == *c ;
+
+  behavior two_same_lt:
+    assumes *a == *b < *c || *a == *c < *b || *b == *c < *a ;
+    ensures *a == *b ;
+
+  behavior two_same_gt:
+    assumes *a == *b > *c || *a == *c > *b || *b == *c > *a ;
+    ensures *b == *c ;
+
+  disjoint behaviors ;
+  // complete behaviors ; Missing case *a != *b && *b != *c && *c != *a
+*/
+void order_3(int* a, int* b, int* c){
+  if(*a > *b){ int tmp = *b ; *b = *a ; *a = tmp ; }
+  if(*a > *c){ int tmp = *c ; *c = *a ; *a = tmp ; }
+  if(*b > *c){ int tmp = *b ; *b = *c ; *c = tmp ; }
+}
+
+
+void test(){
+  int a1 = 5, b1 = 3, c1 = 4 ;
+  order_3(&a1, &b1, &c1) ;
+  //@ assert a1 == 3 && b1 == 4 && c1 == 5 ;
+
+  int a2 = 2, b2 = 2, c2 = 2 ;
+  order_3(&a2, &b2, &c2) ;
+  //@ assert a2 == 2 && b2 == 2 && c2 == 2 ;
+
+  int a3 = 4, b3 = 3, c3 = 4 ;
+  order_3(&a3, &b3, &c3) ;
+  //@ assert a3 == 3 && b3 == 4 && c3 == 4 ;
+
+  int a4 = 4, b4 = 5, c4 = 4 ;
+  order_3(&a4, &b4, &c4) ;
+  //@ assert a4 == 4 && b4 == 4 && c4 == 5 ;
+}
---
+++ code/function-contract/behaviors/ex-6-order-3.c
@@ -0,0 +1,18 @@
+/*@
+  requires \valid(a) && \valid(b) && \valid(c) ;
+  requires \separated(a, b, c);
+
+  assigns *a, *b, *c ;
+
+  ensures *a <= *b <= *c ;
+  ensures { *a, *b, *c } == \old({ *a, *b, *c }) ;
+  
+  ensures \old(*a == *b == *c) ==> *a == *b == *c ;
+  ensures \old(*a == *b < *c || *a == *c < *b || *b == *c < *a) ==> *a == *b ;
+  ensures \old(*a == *b > *c || *a == *c > *b || *b == *c > *a) ==> *b == *c ;
+*/
+void order_3(int* a, int* b, int* c){
+  if(*a > *b){ int tmp = *b ; *b = *a ; *a = tmp ; }
+  if(*a > *c){ int tmp = *c ; *c = *a ; *a = tmp ; }
+  if(*b > *c){ int tmp = *b ; *b = *c ; *c = tmp ; }
+}
