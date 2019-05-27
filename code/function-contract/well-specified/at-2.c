642e8ea75c438e4ec4eabeb212edf329ee1c25aa
Allan Blanchard <allan.blanchard@inria.fr>
2019-05-27 16:06:04 +0200

    One more example for the at function in 3

--- code/function-contract/well-specified/at-2.c
+++ code/function-contract/well-specified/at-2.c
@@ -17,3 +17,10 @@ void example_3(void){
   int *ptr = &x ;
   //@ assert \at(*\at(ptr, Here), L) == 1 ;
 }
+
+/*@ requires x + 2 != p ; */
+void example_4(int* x, int* p){
+  *p = 2 ;
+  //@ assert x[2]  == \at(x[2], Pre) ;
+  //@ assert x[*p] == \at(x[*p], Pre) ;
+}
