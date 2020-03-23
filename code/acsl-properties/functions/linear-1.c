ec78a3324843ceebf0948bc516a14e4da12a66fa
Allan Blanchard <allan.blanchard@cea.fr>
2020-03-23 16:53:34 +0100

    Changes 'linear' example

--- code/acsl-properties/functions/linear-1.c
+++ code/acsl-properties/functions/linear-1.c
@@ -6,23 +6,36 @@
 */
 
 /*@
-  logic integer limit_int_min_ax_b(integer a, integer b) =
-    (a == 0) ? (b > 0) ? INT_MIN : INT_MIN-b :
-    (a <  0) ? (INT_MAX-b)/a :
-               (INT_MIN-b)/a ;
-
-  logic integer limit_int_max_ax_b(integer a, integer b) =
-    (a == 0) ? (b > 0) ? INT_MAX-b : INT_MAX :
-    (a <  0) ? (INT_MIN-b)/a :
-               (INT_MAX-b)/a ;
-*/
-
-/*@
-  requires INT_MIN <= 3 * x ;
-  requires limit_int_min_ax_b(3,4) < x < limit_int_max_ax_b(3,4);
+  requires INT_MIN <= ax_b(3, x, 4) <= INT_MAX;
   assigns \nothing ;
   ensures \result == ax_b(3,x,4); 
 */
 int function(int x){
   return 3*x + 4;
+}
+
+/*@
+  requires INT_MIN <= 3*x ;
+  requires INT_MIN <= ax_b(3, x, 4) <= INT_MAX;
+  assigns \nothing ;
+  ensures \result == ax_b(3,x,4); 
+*/
+int restricted(int x){
+  return 3*x + 4;
+}
+
+/*@
+  requires INT_MIN <= ax_b(3, x, 4) <= INT_MAX;
+  assigns \nothing;
+  ensures \result == ax_b(3,x,4);
+*/
+int function_modified(int x){
+  if(x > 0)
+    return 3 * x + 4;
+  else
+    return 3 * (x + 2) - 2;
+}
+
+void mathematical_example(void){
+  //@ assert ax_b(42, INT_MAX, 1) < ax_b(70, INT_MAX, 1) ;
 }
--- code/acsl-properties/lemmas/linear-0.c
+++ code/acsl-properties/lemmas/linear-0.c
@@ -6,18 +6,6 @@
 */
 
 /*@
-  logic integer limit_int_min_ax_b(integer a, integer b) =
-    (a == 0) ? (b > 0) ? INT_MIN : INT_MIN-b :
-    (a <  0) ? (INT_MAX-b)/a :
-               (INT_MIN-b)/a ;
-
-  logic integer limit_int_max_ax_b(integer a, integer b) =
-    (a == 0) ? (b > 0) ? INT_MAX-b : INT_MAX :
-    (a <  0) ? (INT_MIN-b)/a :
-               (INT_MAX-b)/a ;
-*/
-
-/*@
   lemma ax_b_monotonic_neg:
     \forall integer a, b, i, j ;
       a <  0 ==> i <= j ==> ax_b(a, i, b) >= ax_b(a, j, b);
@@ -31,7 +19,7 @@
 
 /*@
   requires INT_MIN <= a*x <= INT_MAX ;
-  requires limit_int_min_ax_b(a,4) < x < limit_int_max_ax_b(a,4);
+  requires INT_MIN <= ax_b(a,x,4) <= INT_MAX ;
   assigns \nothing ;
   ensures \result == ax_b(a,x,4); 
 */
@@ -43,8 +31,8 @@ int function(int a, int x){
   requires INT_MIN <= a*x <= INT_MAX ;
   requires INT_MIN <= a*y <= INT_MAX ;
   requires a > 0;
-  requires limit_int_min_ax_b(a,4) < x < limit_int_max_ax_b(a,4) ;
-  requires limit_int_min_ax_b(a,4) < y < limit_int_max_ax_b(a,4) ;
+  requires INT_MIN <= ax_b(a,x,4) <= INT_MAX ;
+  requires INT_MIN <= ax_b(a,y,4) <= INT_MAX ;
   assigns \nothing ;
 */
 void foo(int a, int x, int y){
