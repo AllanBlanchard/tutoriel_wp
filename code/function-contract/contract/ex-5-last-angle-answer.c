ab0b3b7f05c75d216a844ea1778bf7673f662f76
Allan Blanchard <allan.blanchard@cea.fr>
2020-03-31 14:27:29 +0200

    [code] Improve an answer to an exercise

--- code/function-contract/contract/ex-5-last-angle-answer.c
+++ code/function-contract/contract/ex-5-last-angle-answer.c
@@ -1,7 +1,10 @@
 /*@
   requires 0 <= first <= 180 ;
   requires 0 <= second <= 180 ;
+  requires first + second <= 180 ;
+
   ensures 180 == first + second + \result ;
+  ensures 0 <= \result <= 180 ;
 */
 int last_angle(int first, int second){
   return 180 - first - second ;
