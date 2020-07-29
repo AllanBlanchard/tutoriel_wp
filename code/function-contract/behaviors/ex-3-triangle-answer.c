8c91e7f00a989713aff183ef6f7e5438b18d8155
Alex Lyr <alex_gfd@hotmail.com>
2020-07-29 15:43:11 +0300

    /function-contract/behviors/ex-3-triangle-answer.c preconditions fix: Insert preconditions for asserting triangle inequality and that a is the hypotenuse of the triangle

--- code/function-contract/behaviors/ex-3-triangle-answer.c
+++ code/function-contract/behaviors/ex-3-triangle-answer.c
@@ -4,6 +4,11 @@ enum Sides { SCALENE, ISOSCELE, EQUILATERAL };
 enum Angles { RIGHT, ACUTE, OBTUSE };
 
 /*@
+  requires a <= b+c && a >= b && a >= c ;
+  requires 0 <= a && a*a <= INT_MAX;
+  requires 0 <= b && b*b <= INT_MAX;
+  requires 0 <= c && c*c <= INT_MAX;
+
   assigns \nothing;
 
   behavior equilateral:
@@ -18,18 +23,18 @@ enum Angles { RIGHT, ACUTE, OBTUSE };
   behavior scalene:
     assumes a != b && a != c && b != c ;
     ensures \result == SCALENE;
-  
+
   disjoint behaviors;
   complete behaviors;
 */
-enum Sides sides_kind(int a, int b, int c){
-  if(a == b && b == c){
-    return EQUILATERAL ;
-  } else if(a == b || b == c || a == c){
-    return ISOSCELE ;
-  } else {
-    return SCALENE ;
-  }
+enum Sides sides_kind(int a, int b, int c) {
+    if (a == b && b == c) {
+        return EQUILATERAL ;
+    } else if (a == b || b == c || a == c) {
+        return ISOSCELE ;
+    } else {
+        return SCALENE ;
+    }
 }
 
 /*@
@@ -37,7 +42,7 @@ enum Sides sides_kind(int a, int b, int c){
   requires 0 <= a && a*a <= INT_MAX;
   requires 0 <= b && b*b <= INT_MAX;
   requires 0 <= c && c*c <= INT_MAX;
-  
+
   assigns \nothing;
 
   behavior obtuse:
@@ -55,13 +60,13 @@ enum Sides sides_kind(int a, int b, int c){
   disjoint behaviors;
   complete behaviors;
 */
-enum Angles angles_kind(int a, int b, int c){
-  if(a*a - b*b > c*c){
-    return OBTUSE;
-  } else if(a*a - b*b < c*c){
-    return ACUTE;
-  } else {
-    return RIGHT;
-  }
+enum Angles angles_kind(int a, int b, int c) {
+    if (a * a - b * b > c * c) {
+        return OBTUSE;
+    } else if (a * a - b * b < c * c) {
+        return ACUTE;
+    } else {
+        return RIGHT;
+    }
 }
 
